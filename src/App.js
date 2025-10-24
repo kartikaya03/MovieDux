06222876a5ef63447dc290d6ede2676143057f67
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2025-10-25 02:57:28 +0530

    first inital commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1 className="header">Welcome to MovieDux</h1>
+      </header>
+      <footer className="footer">
+        <p className="footer">footer content here </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Headers.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css";
+
+export default function Headers() {
+  return (
+    <div className="headers">
+      <img className="logo" src="logo.png" alt="MovieDux" />
+      <h2 className="app-subtitle">
+        It's time for popcorn ! Find your next movie here .
+      </h2>
+    </div>
+  );
+}
